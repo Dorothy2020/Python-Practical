d24994f23a2fef72521e35273ecc8dc47347c8f9
Dorothy_Akoth <dorothy@untapped-global.com>
2023-07-28 15:28:11 +0300

    feat:worked on dates

---
+++ dates.py
@@ -0,0 +1,4 @@
+# 1.How do we convert a python datetime .datetime to excel serial date number
+# 2. Between the two Dataframes that is  pandas and numpy which one is suitable for the creation of single summary spreadsheet
+# creation of one csvs
+# 3.  How to remove timezone information from Datetime object in Python.
