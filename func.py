04d69d8ee51e4b39adfed1f5a3498b30aff37fc7
Dorothy_Akoth <dorothy@untapped-global.com>
2023-07-28 15:21:39 +0300

    feat:for unit testing

---
+++ func.py
@@ -0,0 +1,7 @@
+# create a program for adding a string and write a unittest for it
+def add_str(first_name, last_name):
+    full_name = first_name + " " + last_name
+    return full_name
+
+names = add_str("Dorothy", "Akoth")
+print(names)
